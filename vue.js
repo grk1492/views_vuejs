8d9670aabaad816c624a063189ca25179dab8e44
GraceKitoko <gracekitoko92@gmail.com>
2018-03-08 14:22:38 +0100

    new updates

--- vue.js
+++ vue.js
@@ -38,11 +38,3 @@ const app2 = new Vue({
   }
 })
 
-/*function printTime(){
-  var d = new Date();
-  var hours = d.getHours();
-  var mins = d.getMinutes();
-  var secs = d.getSeconds();
-  document.body.innerHTML = hours+':'+mins+':'+secs;
-}
-setInterval(printTime,1000);*/
